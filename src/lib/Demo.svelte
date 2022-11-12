ce1c94c5b16ae458d0076623306bdd3520cb4804
Doug A <douganderson444@gmail.com>
2022-11-11 20:02:48 -0400

    show Peers

--- src/lib/Demo.svelte
+++ src/lib/Demo.svelte
@@ -5,6 +5,7 @@
 	import Contacts from './Contacts.svelte';
 	import Repo from './Repo.svelte';
 	import Mount from './Mount.svelte';
+	import Peers from './Peers.svelte';
 
 	let wallet;
 	let ownerAddress;
@@ -14,6 +15,7 @@
 	let connect;
 	let username;
 	let data;
+	let peers = new Set();
 
 	onMount(async () => {
 		// browser env, load the library now
@@ -32,15 +34,30 @@
 			handleMsg
 		});
 	}
-	function handleConnect() {}
-	function handleClose() {}
+	function handleConnect(peer) {
+		console.log('handleConnect', peer);
+		// add new peer peer.client_id to set
+		peers = peers.add(peer.client_id);
+	}
+	function handleClose(peer) {
+		console.log('handleClose', peer);
+		// find peer in peers set and remove it
+		peers = peers.delete(peer.client_id);
+	}
 	function handleMsg() {}
 </script>
 
 <WalletManager let:wallet let:ownerAddress let:RSAPublicKey on:Ed25519PublicKey={keyConnect} />
 <div class="flex flex-row h-screen">
-	<Contacts {Ed25519PublicKey} {username} />
-	<div class="flex-1 bg-neutral-700 text-neutral-200">
+	<section class="flex flex-col bg-neutral-100 h-full w-1/3 break-words break-all justify-left">
+		<div class="flex flex-col items-left">
+			My Stuff
+			<div class="ml-2 text-sm">Username</div>
+			<div class="ml-2 text-xs font-mono">{username}</div>
+		</div>
+		<Peers {peers} />
+	</section>
+	<div class="flex-1 w-2/3 bg-neutral-700 text-neutral-200">
 		<Repo let:esModule let:props let:handleChange>
 			<Mount src={esModule} {props} on:change={handleChange} />
 		</Repo>
