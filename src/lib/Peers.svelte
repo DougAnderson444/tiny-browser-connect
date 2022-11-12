2065e911e6510609c64ca282c7a3e44788110f88
Doug A <douganderson444@gmail.com>
2022-11-11 20:02:15 -0400

    add Peers

---
+++ src/lib/Peers.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let peers = [];
+</script>
+
+{#if peers && peers.length}
+	<div class="p-2 bg-neutral-900">
+		Peers
+		{#each peers as peer}
+			<div class="m-1 p-1 flex flex-row items-left text-green-400 text-sm font-mono">
+				{peer}
+			</div>
+		{/each}
+	</div>
+{/if}
