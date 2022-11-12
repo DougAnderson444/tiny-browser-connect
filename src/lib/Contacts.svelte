fee397685a53952d848cd35820120390fdb43d37
Doug A <douganderson444@gmail.com>
2022-11-11 20:02:35 -0400

    Profile >> Contacts

--- src/lib/Contacts.svelte
+++ src/lib/Contacts.svelte
@@ -3,11 +3,5 @@
 	export let username;
 </script>
 
-<section class="flex flex-col bg-neutral-100 h-full w-1/3 break-words break-all justify-left">
-	<div class="flex flex-col items-left">
-		My Stuff
-		<!-- <Profile {Ed25519PublicKey} {username} /> -->
-	</div>
-	<div class="flex flex-col items-left">My Contacts</div>
-	<div class="flex flex-col items-left">Online</div>
-</section>
+<div class="flex flex-col items-left">My Contacts</div>
+<div class="flex flex-col items-left">Online</div>
