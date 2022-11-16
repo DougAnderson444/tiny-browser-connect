bc1c3e6b86b1ea4de2d4fbe8e2052a8e46c6bf80
Doug A <douganderson444@gmail.com>
2022-11-16 15:27:24 -0400

    css

--- src/lib/Demo.svelte
+++ src/lib/Demo.svelte
@@ -58,34 +58,36 @@
 	const handleWallet = (e) => (wallet = e.detail.wallet);
 </script>
 
-<WalletManager
-	on:ownerAddress={handleAddress}
-	on:Ed25519PublicKey={keyConnect}
-	on:wallet={handleWallet}
-/>
+<main class="w-screen">
+	<WalletManager
+		on:ownerAddress={handleAddress}
+		on:Ed25519PublicKey={keyConnect}
+		on:wallet={handleWallet}
+	/>
 
-<div class="text-white">
-	<SideNav let:hideNav>
-		<section class="flex z-50 flex-col  h-full break-words break-all justify-left">
-			<div class="flex flex-col items-left">
-				My Stuff
-				<div class="ml-2 text-sm">Username</div>
-				<div class="ml-2 text-xs font-mono">{username}</div>
+	<div class="text-white">
+		<SideNav let:hideNav>
+			<section class="flex z-50 flex-col  h-full break-words break-all justify-left">
+				<div class="flex flex-col items-left">
+					My Stuff
+					<div class="ml-2 text-sm">Username</div>
+					<div class="ml-2 text-xs font-mono">{username}</div>
+				</div>
+				<Peers {peers} />
+			</section>
+		</SideNav>
+	</div>
+	{#if wallet && ownerAddress}
+		<AppContext {wallet} {ownerAddress} {local}>
+			<div class="flex flex-row min-h-screen h-full">
+				<div class="flex-1 w-2/3 bg-neutral-700 text-neutral-200 pt-16">
+					<Repo let:esModule let:props let:handleChange>
+						<Mount src={esModule} {props} on:change={handleChange} />
+					</Repo>
+				</div>
 			</div>
-			<Peers {peers} />
-		</section>
-	</SideNav>
-</div>
-{#if wallet && ownerAddress}
-	<AppContext {wallet} {ownerAddress} {local}>
-		<div class="flex flex-row min-h-screen h-full">
-			<div class="flex-1 w-2/3 bg-neutral-700 text-neutral-200 pt-16">
-				<Repo let:esModule let:props let:handleChange>
-					<Mount src={esModule} {props} on:change={handleChange} />
-				</Repo>
-			</div>
-		</div>
-	</AppContext>
-{:else}
-	<!-- Local only, no Publish  -->
-{/if}
+		</AppContext>
+	{:else}
+		<!-- Local only, no Publish  -->
+	{/if}
+</main>
