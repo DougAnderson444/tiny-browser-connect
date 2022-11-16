c15e55e1aa7920a04bd543595e823d9237de077a
Doug A <douganderson444@gmail.com>
2022-11-16 15:29:12 -0400

    fix import

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 import * as b64 from '@stablelib/base64';
-import { hash } from '@stablelib/sha256';
+import * as sha256 from '@stablelib/sha256';
 
 export function getArweaveAddress(jwk) {
 	// get uint8array of the string
@@ -13,7 +13,7 @@ export function getArweaveAddress(jwk) {
 
 export function getAddress(Ed25519PublicKey) {
 	// get the isomorphic hash of the asset as the tag
-	const hashed = hash(new Uint8Array(Ed25519PublicKey)); // make tag unique, so that it can go to a new owner without conflict
+	const hashed = sha256.hash(new Uint8Array(Ed25519PublicKey)); // make tag unique, so that it can go to a new owner without conflict
 	const hashB64 = b64.encodeURLSafe(new Uint8Array(hashed)).replace('=', '');
 	return hashB64;
 }
